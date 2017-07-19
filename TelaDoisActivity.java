5fa9cb9d6e032885348933316ea1fe3866210fde
lucascostadev1996 <lucas.alves.costa96@gmail.com>
2017-07-19 11:20:03 -0300

    Update TelaDoisActivity.java

--- TelaDoisActivity.java
+++ TelaDoisActivity.java
@@ -3,8 +3,10 @@ package br.com.lucasalves.aula01layout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class TelaDoisActivity extends AppCompatActivity {
 
@@ -29,6 +31,25 @@ public class TelaDoisActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(
+                R.menu.menu_exemplo,    //menu XML que vamos converter em submenu
+                menu                    //objeto Menu que recebe os dados de XML
+        );
+        //Retornar true ou false. Se true exibe o menu, se false esconde  menu
+        //O método super.onCreateOptionsMenu(menu) sempre retorna true
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_novo:
+                Toast.makeText(getBaseContext(),"Cliquei Aqui!",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu_voltar:
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
